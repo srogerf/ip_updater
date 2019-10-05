2b46fe39262efe848273b49976d6dc7c5841ec36
roger <srogerf@gmail.com>
2019-10-05 11:02:17 -0700

    inital

--- ip_updater.go
+++ ip_updater.go
@@ -11,25 +11,26 @@ func main() {
     log.Print("Starting IP importer")
     api_key := flag.String("key", "none", "godaddy api key")
     api_secret := flag.String("secret", "none", "godaddy api secret")
-    domain := flag.String("doamin", "none", "doamin to update")
+    domain := flag.String("domain", "none", "domain to update")
     host := flag.String("host", "none", "host to update")
+    flag.Parse()
 
-    ipv4 := address.GetIPv4()
-    ipv6 := address.GetIPv6()
+//get current ip values
+    assigned_ipv4 := address.GetIPv4()
+//    assigned_ipv6 := address.GetIPv6()
 
-    log.Printf("\n    IP4: %s\n    IP6: %s\n\n", ipv4, ipv6)
+//get dns ip values
+    dns_ipv4 := provider.GetIPv4(*domain, *host, *api_key, *api_secret)
 
-    currentIPv4 := provider.GetIPv4(*domain, *host, *api_key, *api_secret)
-    log.Printf("\n    current IP4: %s\n    IP6: \n\n", currentIPv4)
-//    provider.godaddy.GetIPv4()
-// get current record
-//    updater.SelectProvider("godaddy")
-//    updater.SelectRecord("test.com")
-//    ipv4 = updater.getCurrentRecord()
+    log.Printf("\n    assigned IP4: %s\n    dns ip v4: %s\n\n", assigned_ipv4, dns_ipv4)
 
 
 //check if right
-
+    if assigned_ipv4 != dns_ipv4 {
+        log.Println("IPv4 requires update")
+    } else {
+        log.Println("IPv4 does not require update")
+    }
 
 //update if out of date
 
--- provider/go_daddy.go
+++ provider/go_daddy.go
@@ -3,28 +3,58 @@ package provider
 import (
     "log"
     "net/http"
-    "io/ioutil"
-    "strings"
+     "io/ioutil"
+    "time"
+    "encoding/json"
 )
 // test to check api
-// curl -X GET -H"Authorization: sso-key ${API_KEY}:${API_SECRET}" "https://api.godaddy.com/v1/domains/${DOMAIN}/records/A/${DOMAIN}"
+// curl -X GET -H"Authorization: sso-key ${API_KEY}:${API_SECRET}" "https://api.godaddy.com/v1/domains/${DOMAIN}/records/A/"
 
 //d "[ { \"data\": \"$currentIp\", \"port\": $port, \"priority\": 0, \"protocol\": \"string\", \"service\": \"string\", \"ttl\": $ttl, \"weight\": $weight } ]"
 
+//update the ip address
 func UpdateIPv4() {
+//    checkUri := "https://api.godaddy.com/v1/domains/" + domain + "/records/A/"
+//    sso := "sso-key " + api_key + ":" + api_secret
 }
 
 // get the current ip of the host from godaddy given the domain and host
 func GetIPv4(domain string, host string, api_key string, api_secret string) (string) {
-    checkUri := "https://api.godaddy.com/v1/domains"
-    res, err := http.Get(checkUri)
+    checkUri := "https://api.godaddy.com/v1/domains/" + domain + "/records/A/"
+    sso := "sso-key " + api_key + ":" + api_secret
 
+    req, err := http.NewRequest("GET", checkUri, nil)
+    if err != nil  {
+      log.Println("failed")
+    }
+
+    req.Header.Set("Accept", "application/json")
+    req.Header.Set("Content-Type", "application/json")
+    req.Header.Set("Authorization", sso)
+
+    client := &http.Client{Timeout: time.Second * 10}
+    res, err := client.Do(req)
     if err != nil  {
       log.Println("failed")
     }
     defer res.Body.Close()
 
-      body, _ := ioutil.ReadAll(res.Body)
-      ipv4 := strings.Split(string(body), ",")[1]
-      return ipv4
+    body, _ := ioutil.ReadAll(res.Body)
+
+//define return and parse body of html
+    type ARecord struct {
+         Data  string `json:"data"`
+         Name  string `json:"name"`
+         Ttl  int `json:ttl"`
+         Record_type  string `json:type"`
+    }
+    var data []ARecord
+    err = json.Unmarshal([]byte(body), &data);
+
+    if err != nil  {
+        log.Println("failed %d\n", err)
+    }
+
+    log.Printf("current ip:  %s\n", data[0].Data)
+    return data[0].Data
 }
