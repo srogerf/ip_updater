fc3675e4c3e7299de3ac4c5927df2f7a4df8787c
roger <srogerf@gmail.com>
2020-02-10 19:21:14 -0800

    some code cleanup

--- frontend/dns-manager/src/ProviderData.js
+++ frontend/dns-manager/src/ProviderData.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReButton from './riff/ReButton';
+import GoDaddyDetails from "./providers/GoDaddy";
 import {
   Button,
   Dropdown,
@@ -8,51 +8,8 @@ import {
   FormControl,
   DropdownButton
 } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-class GoDaddyDetails extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-      key: '',
-      secret: '',
-      domain: '',
-      host: '*'
-    }
-    // This binding is necessary to make `this` work in the callback
-//    this.handleClick = this.handleClick.bind(this);
-//    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange = (e) => {
-    console.log("change ", e, " times ", this.state.count);
-    
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  handleClick = () => {
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("up");
-  }
-
-  render() {
-    return (
-      <div>
-        <ReButton labelText="API Key" defaultText="Remote key to access REST API"
-            onChange={(e) => this.handleChange(e)}/>
-        <ReButton labelText="API Secret" defaultText="Remote secret to access REST API"
-                  onChange={this.handleChange}/>
-        <ReButton labelText="DNS Domain" defaultText="Domain name to update"
-                  onChange={this.handleChange}/>
-        <ReButton labelText="DNS Host" defaultText="Host name to update"
-            onChange={this.handleChange}/>
-      </div>
-    )
-  }
-}
-export default GoDaddyDetails;
 
 ReactDOM.render(
   <GoDaddyDetails/>,
--- frontend/dns-manager/src/index.js
+++ frontend/dns-manager/src/index.js
@@ -1,6 +1,5 @@
 //import React from 'react';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import './index.css';
 import './ProviderData';
 
 /*
